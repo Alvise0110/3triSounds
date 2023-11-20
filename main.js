a4c81fb90132c5ea7f8335a66b1d7b0d9de2bb21
Gabriel Alvise <107253327+Alvise0110@users.noreply.github.com>
2023-11-20 10:01:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let contador = 0;
 while(contador < listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_"+efeito;
-    listaDeTeclas [contador] .onclick = function (){;
+    listaDeTeclas[contador].onclick = function (){;
        tocaSom(idAudio)
     }   
     contador = contador +1;
-    console.log(contador);
+    
 }
